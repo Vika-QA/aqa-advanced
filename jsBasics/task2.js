eca9397292258c8a660eef79d80de82dbdf0c83e
Vika-QA <vict.kiseleva@gmail.com>
2024-08-30 00:47:15 +0200

    feat(task2):add a greeting concatenation and a template string

---
+++ jsBasics/task2.js
@@ -0,0 +1,7 @@
+const firstName = "Vika";
+const secondName = "QA";
+const greeting = "Hello" + " " + firstName + " " + secondName;
+console.log(greeting);
+
+const anotherGreeting = `Hello ${firstName} ${secondName}`;
+console.log(anotherGreeting);
