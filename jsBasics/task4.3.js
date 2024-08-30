c5d0de0582ac7b9c9daf67d7b6291607d048b1f8
Vika-QA <vict.kiseleva@gmail.com>
2024-08-30 22:43:37 +0200

    refactor(task4.3): minor changes, delete deprecated math.pow method

--- jsBasics/task4.3.js
+++ jsBasics/task4.3.js
@@ -1,16 +1,5 @@
-const radius = 12;
-const height = 10;
-const pi = Math.PI;
-const radiusSquare = Math.pow(radius, 2);
-const volumeCylinder = +(pi * radiusSquare * height).toFixed(2);
+const radius = 4;
+const height = 7;
+const cylinderVolume = +(Math.PI * radius ** 2 * height).toFixed(2);
 
-console.log(volumeCylinder);
-
-// another way
-
-const newRadius = 4;
-const newHeight = 7;
-const newPi = Math.PI;
-const newVolumeCylinder = +(pi * newRadius ** 2 * height).toFixed(2);
-
-console.log(newVolumeCylinder);
+console.log(cylinderVolume);
