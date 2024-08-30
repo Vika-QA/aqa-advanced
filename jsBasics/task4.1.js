2cb15f894bb00172b0ccd2a61b85b3249d736981
Vika-QA <vict.kiseleva@gmail.com>
2024-08-30 22:40:57 +0200

    refactor(task4.1): minor changes, delete deprecated math.pow method

--- jsBasics/task4.1.js
+++ jsBasics/task4.1.js
@@ -1,13 +1,3 @@
 const radius = 5;
-const pi = Math.PI;
-const radiusSquare = Math.pow(radius, 2);
-const squareRound = +(pi * radiusSquare).toFixed(2);
-
-console.log(squareRound);
-
-// another way
-const newRadius = 7;
-const newPi = Math.PI;
-const newSquareRound = +(pi * newRadius ** 2).toFixed(2);
-
-console.log(newSquareRound);
+const circleArea = +(Math.PI * radius ** 2).toFixed(2);
+console.log(circleArea);
