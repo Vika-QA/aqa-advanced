48968d12676c42ecf07e5af2d87ddd319009dc82
Vika-QA <vict.kiseleva@gmail.com>
2024-09-11 00:26:30 +0200

    feat(task1): calculation rectangle area function

---
+++ lesson6/task1.js
@@ -0,0 +1,18 @@
+//function declaration
+function calculateRectangleArea1(width, height) {
+  return width * height;
+}
+console.log(calculateRectangleArea1(5, 10));
+
+//function expression
+const calculateRectangleArea2 = function (width, height) {
+  return width * height;
+};
+const rectangleArea = calculateRectangleArea2(5, 10);
+console.log(rectangleArea);
+
+//arrow function
+const calculateRectangleArea3 = (width, height) => {
+  return width * height;
+};
+console.log(calculateRectangleArea3(5, 10));
