7f04948db964b739c43f585d9ec2059001a0870b
Vika-QA <vict.kiseleva@gmail.com>
2024-09-22 23:54:06 +0200

    feat(task1): information about students

---
+++ lesson9/task1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firstName = "Viktoriia";
+student.lastName = "Kiselova";
+student.age = 31;
+student.courses = ["Math", "History", "Programming"];
+
+console.log(student);
