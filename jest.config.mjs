9c5b4fb91b82ff42de386874a90da1761da9c8ac
Vika-QA <vict.kiseleva@gmail.com>
2024-10-12 23:12:32 +0200

    feat: install axios, cross-env; refactor jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -156,10 +156,10 @@ const config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/api-testing/**/*.js",
+    // "**/?(*.)+(spec|test).js",
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
