0c5e49d62c0d15b1ec18ca06633fa268aab83701
Viktoriia Kiselova <vict.kiseleva@gmail.com>
2024-12-25 21:00:05 +0100

    feat(cypress): minor changes #1

--- lesson17/e2e/lesson18/constants.js
+++ lesson17/e2e/lesson18/constants.js
@@ -8,5 +8,5 @@ export const visitSiteWithAuth = () => {
 };
 
 // user already logged with this email, so if you want to create new one, pls change the email
-export const email = "t1estsupertestwoooow@qwe.qwe";
+export const email = "t1aaaefg025hjkestsupertestwoooow@qwe.qwe";
 export const password = "Test123!";
